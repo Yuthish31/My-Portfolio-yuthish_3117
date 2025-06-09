5a475b69b5d15563b2172b2e66362056f1a7703b
Yuthish31 <yuthish2222k0156@gmail.com>
2025-06-08 23:51:44 -0700

    Update portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="footer">
       <p>&copy; {currentYear} Yuthish-3117. All rights reserved.</p>
